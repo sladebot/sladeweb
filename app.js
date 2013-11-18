84b3ad5f1ca668050ca5286b4763745b184ba735
Souranil Sen <souranil.sen@tcs.com>
2013-11-18 19:30:56 +0530

    Added nodeman and redis
    * Adding nodeman so that we dont have to restart everytime:
    
      Start with: nodeman app.js
    * Added redis

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var redis = require('redis');
 
 var app = express();
+// Creating redis connections
+var db = redis.createClient();
+
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -25,7 +29,19 @@ app.use(express.session());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(function(req, res, next){
+  var ua = req.headers['user-agent'];
+  db.zadd('online', Date.now(), ua, next); 
+});
+app.use(function(req, res, next){
+  var min = 60 * 1000;
+  var ago = Date.now() - min;
+  db.zrevrangebyscore('online', '+inf', ago, function(err, users){
+    if(err) return next(err);
+    req.online = users;
+    next();
+  });
+});
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
